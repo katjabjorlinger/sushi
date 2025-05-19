88b8ce049b825eeba8f5bf339262628f978fbf0e
katjabjorlinger <130973201+katjabjorlinger@users.noreply.github.com>
2025-05-19 16:43:32 +0200

    navbar

--- Frontend/components/navigation.js
+++ Frontend/components/navigation.js
@@ -1,46 +1,48 @@
-// components/navigation.js
 export default {
   name: "Navigation",
+  data() {
+    return { lastScrollY: 0, isHidden: false, isSolid: false };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener("scroll", this.onScroll);
+  },
+  methods: {
+    onScroll() {
+      const currentScroll = window.scrollY;
+      this.isHidden = currentScroll > this.lastScrollY && currentScroll > 100
+                     ? true
+                     : (currentScroll < this.lastScrollY ? false : this.isHidden);
+      this.isSolid = currentScroll > 50;
+      this.lastScrollY = currentScroll;
+    },
+  },
   template: `
-   <nav
-  class="navbar navbar-expand-md navbar-light pt-5 pb-4 sticky-top"
-  style="background-color: none"
->
-  <div class="container-xxl">
-    <router-link class="navbar-brand" to="/">
-      <h1 class="h2">Omakase</h1>
-    </router-link>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#main-nav"
-      aria-controls="main-nav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
+    <nav
+      :class="[
+        'navbar',
+        'navbar-expand-md', 'text-white',
+        isSolid
+          ? 'fixed-top bg-dark navbar-dark'
+          : 'position-absolute bg-transparent navbar-dark',
+        isHidden ? 'translate-up' : '' 
+      ]"
     >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div
-      class="collapse navbar-collapse justify-content-end align-center"
-      id="main-nav"
-    >
-      <ul class="navbar-nav">
-        <!-- Här är länkar till respektive route -->
-
-        <li class="nav-item">
-          <a href="#/" @click.prevent="$router.push('/')">Home</a> |
-        </li>
-        <li class="nav-item">
-          <a href="#/menu" @click.prevent="$router.push('/menu')">Menu</a> |
-        </li>
-        <li class="nav-item">
-          <a href="#/booking" @click.prevent="$router.push('/booking')"
-            >Booking</a
-          >
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-`, };
+      <div class="container-xxl">
+        <router-link class="navbar-brand" to="/"><h1 class="h2 text-white">Omakase</h1></router-link>
+        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#main-nav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="main-nav">
+          <ul class="navbar-nav ">
+            <li class="nav-item"><a class="nav-link" href="#/" @click.prevent="$router.push('/')">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="#/menu" @click.prevent="$router.push('/menu')">Menu</a></li>
+            <li class="nav-item"><a class="nav-link" href="#/booking" @click.prevent="$router.push('/booking')">Booking</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  `,
+};
